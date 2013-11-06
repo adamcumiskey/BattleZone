8b8c5a94fc1bc88ca3c00947f77b3a07d15783cf
adamcumiskey <adam.cumiskey@gmail.com>
2013-11-06 18:52:06 -0500

    Initial Commit

---
+++ BattlezoneClone/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BattlezoneClone
+//
+//  Created by Adam Cumiskey on 11/6/13.
+//  Copyright (c) 2013 Adam Cumiskey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
