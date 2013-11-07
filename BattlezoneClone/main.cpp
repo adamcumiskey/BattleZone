3093f971c148a76990e03933c01085206a07842e
adamcumiskey <adam.cumiskey@gmail.com>
2013-11-07 17:40:09 -0500

    The infinite plane is rendering, however I need to find a way for it to only display a line at the horizon. I also added in a camera class to control movement. We'll see if I'm allowed to keep it.

--- BattlezoneClone/main.cpp
+++ BattlezoneClone/main.cpp
@@ -8,6 +8,7 @@
 //  Main file for the battlezone program. All OpenGL main loop logic should go here.
 
 #include <iostream>
+#include "Camera.h"
 
 #ifdef __APPLE__
 #  include <GLUT/glut.h>
@@ -17,7 +18,8 @@
 
 #define TABLE_WIDTH 4000
 #define TABLE_HEIGHT 2000
-static float Xangle = 0.0, Yangle = 0.0, Zangle = 0.0; // Angles to rotate scene.
+
+CCamera Camera(0.0, 0.5, 0.0);
 
 void setup()
 {
@@ -30,9 +32,9 @@ void drawGround()
 {
     float origin[4] = {0, 0, 0, 1};
     float one[4] = {1, 0, 0, 0};
-    float two[4] = {0, 1, 0, 0};
+    float two[4] = {0, 0, 1, 0};
     float three[4] = {-1, 0, 0, 0};
-    float four[4] = {0, -1, 0, 0};
+    float four[4] = {0, 0, -1, 0};
     
     glColor3f(0.0, 1.0, 0.0);
     glBegin(GL_TRIANGLES);
@@ -58,15 +60,11 @@ void drawGround()
 
 void drawScene()
 {
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_BACK, GL_LINE);
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    glTranslatef(0.0, -1.0, -10.0);
     
-    // Rotate scene.
-    glRotatef(Zangle, 0.0, 0.0, 1.0);
-    glRotatef(Yangle, 0.0, 1.0, 0.0);
-    glRotatef(Xangle, 1.0, 0.0, 0.0);
+    Camera.Render();
     
     drawGround();
     
@@ -78,7 +76,7 @@ void resize(int w, int h)
     glViewport(0, 0, (GLsizei)w, (GLsizei)h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(25.0f, 2.0, 5, 500);
+    gluPerspective(25.0f, 2.0, 1, 500);
     
     glMatrixMode(GL_MODELVIEW);
 }
@@ -90,42 +88,22 @@ void keyInput(unsigned char key, int x, int y)
         case 27:
             exit(0);
             break;
-        case ' ':
-            glutPostRedisplay();
+        case 'w':
+            Camera.MoveForward(-.1);
             break;
-        case 'x':
-            Xangle += 5.0;
-            if (Xangle > 360.0) Xangle -= 360.0;
-            glutPostRedisplay();
+        case 'a':
+            Camera.RotateY(5.0);
             break;
-        case 'X':
-            Xangle -= 5.0;
-            if (Xangle < 0.0) Xangle += 360.0;
-            glutPostRedisplay();
+        case 's':
+            Camera.MoveForward(.1);
             break;
-        case 'y':
-            Yangle += 5.0;
-            if (Yangle > 360.0) Yangle -= 360.0;
-            glutPostRedisplay();
-            break;
-        case 'Y':
-            Yangle -= 5.0;
-            if (Yangle < 0.0) Yangle += 360.0;
-            glutPostRedisplay();
-            break;
-        case 'z':
-            Zangle += 5.0;
-            if (Zangle > 360.0) Zangle -= 360.0;
-            glutPostRedisplay();
-            break;
-        case 'Z':
-            Zangle -= 5.0;
-            if (Zangle < 0.0) Zangle += 360.0;
-            glutPostRedisplay();
+        case 'd':
+            Camera.RotateY(-5.0);
             break;
         default:
             break;
     }
+    glutPostRedisplay();
 }
 
 void printInteraction()
